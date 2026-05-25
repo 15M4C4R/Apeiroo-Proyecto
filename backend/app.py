32efba52126955ab0ca3c03d123e0c6b3d2a54d1
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-05-25 17:34:57 +0200

    generalización ruta post

--- backend/app.py
+++ backend/app.py
@@ -4,21 +4,24 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/hola')
-def hola():
-    return jsonify({"mensaje": "¡Conexión exitosa con Flask!"})
-
-@app.route('/api/rendimiento/ubuntu', methods=['POST'])
+@app.route('/api/rendimiento/', methods=['POST'])
 def recibir_rendimiento_ubuntu():
     data = request.json
     
     if not data:
         return jsonify({"error": "No se han proporcionado datos"}), 400
     
-    print(f"CPU de la VM (Ubuntu): {data.get('cpu_uso')}%")
-    print(f"RAM en uso: {data.get('ram_uso')}%")
+    if data.get("vm_id") is "servidor-ubuntu":
+        print("VM Ubuntu")
+        print(f'CPU de la VM (Ubuntu): {data.get("cpu_uso")}%')
+        print(f'RAM en uso: {data.get("ram_uso")}%')
+    
+    elif data.get("vm_id") == "servidor-debian":
+        print("VM Debian")
+        print(f'CPU de la VM (Debian): {data.get("cpu_uso")}%')
+        print(f'RAM en uso: {data.get("ram_uso")}%')
     
     return jsonify({"mensaje": "Datos recibidos correctamente"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)

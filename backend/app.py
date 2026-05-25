8ada0c627a497a72cd2b6eda7dc3c0bd8117d660
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-05-25 13:17:26 +0200

    función para recoger los datos

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,18 @@ CORS(app)
 @app.route('/api/hola')
 def hola():
     return jsonify({"mensaje": "¡Conexión exitosa con Flask!"})
+
+@app.route('/api/rendimiento/ubuntu', methods=['POST'])
+def recibir_rendimiento_ubuntu():
+    data = request.json
+    
+    if not data:
+        return jsonify({"error": "No se han proporcionado datos"}), 400
+    
+    print(f"CPU de la VM (Ubuntu): {data.get('cpu_uso')}%")
+    print(f"RAM en uso: {data.get('ram_uso')}%")
+    
+    return jsonify({"mensaje": "Datos recibidos correctamente"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
